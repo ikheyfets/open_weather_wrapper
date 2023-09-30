a6f5453d96df21da461f91b18fe761d550c058ed
Igor Kheyfets <i.a.kheyfets@gmail.com>
2023-09-29 20:25:40 -0400

    refactor

--- api_call.py
+++ api_call.py
@@ -5,24 +5,29 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 class Forecast():
-    def __init__(self, zip_code) -> None:
+    def __init__(self, ) -> None:
         load_dotenv()
-        self.zip_code = str(zip_code)
         self.api_key = os.getenv('API_KEY')
-        self.geocode_zip_to_coords()
 
-
-    def geocode_zip_to_coords(self):
-        api_call = f'http://api.openweathermap.org/geo/1.0/zip?zip={self.zip_code},US&limit=1&appid={self.api_key}'
+    def geocode_zip_to_coords(self, zip_code):
+        api_call = f'http://api.openweathermap.org/geo/1.0/zip?zip={zip_code},US&limit=1&appid={self.api_key}'
         response = requests.get(api_call).json()
-        self.lat = response['lat']
-        self.lon = response['lon']
+        lat = response['lat']
+        lon = response['lon']
+        return lat, lon
     
-    def get(self):
-        api_call = f'https://api.openweathermap.org/data/3.0/onecall?lat={self.lat}&lon={self.lon}&exclude=current,minutely,hourly,alerts&appid={self.api_key}'
+    def get(self, zip_code):
+        lat, lon = self.geocode_zip_to_coords(zip_code)
+
+        api_call = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly,alerts&appid={self.api_key}'
         response = requests.get(api_call).json()
-        
 
         for day in response['daily']:
             unix_timestamp = day['dt']
-            print(f"{datetime.utcfromtimestamp(unix_timestamp).strftime('%Y-%m-%d')}\t{day['temp']['min']}/{day['temp']['max']}\t{day['rain']}")
+            # Rain check
+            if 'rain' not in day.keys():
+                rain = 0
+            else:
+                rain = day['rain']
+
+            print(f"{datetime.utcfromtimestamp(unix_timestamp).strftime('%Y-%m-%d')}\t{day['temp']['min']}/{day['temp']['max']}\t{rain}")
