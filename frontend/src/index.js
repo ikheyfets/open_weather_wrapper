7c200c73e7f47824e02e2a0b7aa0732d5e0623cc
Igor Kheyfets <i.a.kheyfets@gmail.com>
2023-10-03 12:16:55 -0400

    refactor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,13 @@ import { render } from 'react-dom';
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Header from "./components/Header";
+import Forecast from "./components/Forecast";
 
 function App() {
   return (
     <ChakraProvider>
       <Header />
+      <Forecast />
     </ChakraProvider>
   )
 }
